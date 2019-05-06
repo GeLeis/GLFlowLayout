41c7ece2b7b304f3b4981f6005cd7fe3a2d20b05
gelei <gelei@anve.com>
2019-05-06 17:14:02 +0800

    提交GLFlowLayout 1.0.0版本,并附上demo

---
+++ GLFlowLayoutDemo/SectionHeaderReusableView.h
@@ -0,0 +1,17 @@
+//
+//  SectionHeaderReusableView.h
+//  GLFlowLayoutDemo
+//
+//  Created by gelei on 2019/5/6.
+//  Copyright © 2019 gelei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SectionHeaderReusableView : UICollectionReusableView
+
+@end
+
+NS_ASSUME_NONNULL_END
